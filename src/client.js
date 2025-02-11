860dc6b087a24e3e2bb3a7fde815b5a8689256c5
huilin <ejk2827@rit.edu>
2025-02-11 10:16:00 -0500

    eslint errors fixed

--- src/client.js
+++ src/client.js
@@ -1,51 +1,51 @@
 const handleResponse = async (response) => {
-    const contentType = response.headers.get('Content-Type');
+  const contentType = response.headers.get('Content-Type');
 
-    const text = await response.text();
-    console.log('Raw Response: ', text);
+  const text = await response.text();
+  console.log('Raw Response: ', text);
 
-    let parsedData;
+  let parsedData;
 
-    if (contentType.includes('application/json')) {
-        parsedData = JSON.parse(text);
-    } else if (contentType.includes('text/xml')) {
-        const parser = new DOMParser();
-        parsedData = parser.parseFromString(text, 'text/xml');
-    } else {
-        console.error('Unsupported');
-        return;
-    }
+  if (contentType.includes('application/json')) {
+    parsedData = JSON.parse(text);
+  } else if (contentType.includes('text/xml')) {
+    const parser = new DOMParser();
+    parsedData = parser.parseFromString(text, 'text/xml');
+  } else {
+    console.error('Unsupported');
+    return;
+  }
 
-    displayResponse(parsedData, contentType);
-}
+  displayResponse(parsedData, contentType);
+};
 
 const displayResponse = (data, contentType) => {
-    const contentDiv = document.querySelector('#content');
+  const contentDiv = document.querySelector('#content');
 
-    contentDiv.innerHTML = "";
-    let html = "";
+  contentDiv.innerHTML = '';
+  let html = '';
 
-    html += `<h2>${document.getElementById('page').value}</h2>`
-    html += `<h3>${data.message}</h3>`;
+  html += `<h2>${document.getElementById('page').value}</h2>`;
+  html += `<h3>${data.message}</h3>`;
 
-    contentDiv.innerHTML = html;
+  contentDiv.innerHTML = html;
 };
 
 const sendRequest = async () => {
-    const page = document.getElementById('page').value;
-    const type = document.getElementById('type').value;
+  const page = document.getElementById('page').value;
+  const type = document.getElementById('type').value;
 
-    console.log("test");
-    try {
-        const response = await fetch(page, {
-            headers: {
-                Accept: type
-            }
-        })
-        handleResponse(response);
-    } catch(error) {
-        console.error("Fetch error: ", error);
-    }  
+  console.log('test');
+  try {
+    const response = await fetch(page, {
+      headers: {
+        Accept: type,
+      },
+    });
+    handleResponse(response);
+  } catch (error) {
+    console.error('Fetch error: ', error);
+  }
 };
 
 document.getElementById('send').addEventListener('click', sendRequest);
--- src/responses.js
+++ src/responses.js
@@ -1,107 +1,94 @@
 const fs = require('fs');
-const path = require('path');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
+const respond = (request, response, content, type, status) => {
+  // Set status code and content type
+  response.writeHead(status, {
+    'Content-Type': type,
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
+
+  response.write(content);
+  response.end();
+};
+
 const getIndex = (request, response) => {
-  respond(request, response, index, 'text/html', 200)
+  respond(request, response, index, 'text/html', 200);
 };
 
 const getCSS = (request, response) => {
-  respond(request, response, css, 'text/css', 200)
-}
+  respond(request, response, css, 'text/css', 200);
+};
 
-const respond = (request, response, content, type, status) => {
-    
-    // Set status code and content type
-    response.writeHead(status, {
-        'Content-Type': type,
-        'Content-Length': Buffer.byteLength(content, 'utf8')
-    });
+const getData = (request, response, message, status, id = null) => {
+  // XML handling
+  if (request.acceptedTypes[0] === 'text/xml') {
+    let xmlResponse = `<response><message>${message}</message>`;
+    if (id) xmlResponse += `<id>${id}</id>`;
+    xmlResponse += '</response>';
 
-    response.write(content);
-    response.end();
-}
-
-const getData = (request, response, message, status, id=null) => {
-
-    // XML handling
-    if (request.acceptedTypes[0] === 'text/xml') {
-        let xmlResponse = `<response><message>${message}</message>`;
-        if (id) xmlResponse += `<id>${id}</id>`;
-        xmlResponse += `</response>`;
-
-        return respond(request, response, xmlResponse, 'text/xml', status);
-    } else {
-        let jsonResponse = {
-            message: message
-        }
-        if(id) jsonResponse.id = id;
-        console.log(jsonResponse);
-        return respond(request, response, JSON.stringify(jsonResponse), 'application/json', status);
-    }
-}
+    return respond(request, response, xmlResponse, 'text/xml', status);
+  }
+  const jsonResponse = {
+    message,
+  };
+  if (id) jsonResponse.id = id;
+  console.log(jsonResponse);
+  return respond(request, response, JSON.stringify(jsonResponse), 'application/json', status);
+};
 
 const getSuccess = (request, response) => {
-    getData(request, response, "This is a successful response", 200);
-}
+  getData(request, response, 'This is a successful response', 200);
+};
 
 const getBadRequest = (request, response) => {
+  let status = 200;
+  let message = 'This request has the required parameters';
+  let id = null;
 
-    let status = 200;
-    let message = "This request has the required parameters";
-    let id = null;
+  if (!request.query.valid || request.query.valid !== 'true') {
+    // set error message
+    message = 'Missing valid query parameter set to true';
+    status = 400;
+    id = 'badRequest';
+    return getData(request, response, message, status, id);
+  }
 
-    if (!request.query.valid || request.query.valid !== 'true') {
-        // set error message
-        message = "Missing valid query parameter set to true"
-        status = 400;
-        id= 'badRequest';
-        return getData(request, response, message, status, id);
-    }
-
-    return getData(request, response, message, status);
-}
+  return getData(request, response, message, status);
+};
 
 const getUnauthorized = (request, response) => {
-    let status = 200;
-    let message = "This request has the required parameters";
-    let id = null;
+  let status = 200;
+  let message = 'This request has the required parameters';
+  let id = null;
 
-    if (!request.query.loggedIn || request.query.loggedIn !== 'yes') {
-        message="Missing loggedIn query parameter set to yes";
-        status = 401;
-        id="unauthorized";
-    }
-    return getData(request, response, message, status);
-
-}
-
-const getForbidden = (request, response) => {
-    return getData(request, response, "You do not have access to this content", 403), 'forbidden';
-}
-
-const getInternal = (request, response) => {
-    return getData(request, response, "Internal Server Error. Something went wrong.", 500, 'internalError')
-}
-
-const getNotImplemented = (request, response) => {
-    return getData(request, response, "A get request for this page has not been implemented yet. Check again later for updated content", 501, 'notImplemented');
-}
-
-const getNotFound = (request, response) => {
-    return getData(request, response, "The page you are looking for was not found", 404, 'notFound')
-}
-
-  module.exports = {
-    getIndex,
-    getCSS,
-    getSuccess,
-    getBadRequest,
-    getUnauthorized,
-    getForbidden,
-    getInternal,
-    getNotImplemented,
-    getNotFound
+  if (!request.query.loggedIn || request.query.loggedIn !== 'yes') {
+    message = 'Missing loggedIn query parameter set to yes';
+    status = 401;
+    id = 'unauthorized';
   }
+  if (!id) { return getData(request, response, message, status); }
+  return getData(request, response, message, status, id);
+};
+
+const getForbidden = (request, response) => getData(request, response, 'You do not have access to this content', 403, 'forbidden');
+
+const getInternal = (request, response) => getData(request, response, 'Internal Server Error. Something went wrong.', 500, 'internalError');
+
+const getNotImplemented = (request, response) => getData(request, response, 'A get request for this page has not been implemented yet. Check again later for updated content', 501, 'notImplemented');
+
+const getNotFound = (request, response) => getData(request, response, 'The page you are looking for was not found', 404, 'notFound');
+
+module.exports = {
+  getIndex,
+  getCSS,
+  getSuccess,
+  getBadRequest,
+  getUnauthorized,
+  getForbidden,
+  getInternal,
+  getNotImplemented,
+  getNotFound,
+};
